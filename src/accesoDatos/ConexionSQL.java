aa00fcb7b69eda5effd58fd94b4a585d64abf533
sebastianchennavarro <sebastianchennavarro@gmail.com>
2025-03-17 21:28:57 -0600

    Comment

--- src/accesoDatos/ConexionSQL.java
+++ src/accesoDatos/ConexionSQL.java
@@ -5,14 +5,13 @@ package accesoDatos;
 import java.sql.Connection;      // Representa la conexión con la base de datos
 import java.sql.DriverManager;  // Permite obtener la conexión con SQL Server
 import java.sql.SQLException;   // Maneja errores de SQL
-
+//Importe a la base de datos 123 Sebastian Test 
 // Clase para manejar la conexión con SQL Server
 public class ConexionSQL {
     
     // URL de conexión a SQL Server
     // Contiene los datos necesarios para conectarse a la base de datos
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=cine;user=sa;password=P@ssword#2026;encrypt=false;trustServerCertificate=true";
-
+    private static final String URL = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=cine;user=sa;password=Tamara28;encrypt=false;trustServerCertificate=true";
     /**
      * Método estático para establecer la conexión con la base de datos.
      * @return Devuelve un objeto `Connection` si la conexión es exitosa, de lo contrario devuelve `null`.
